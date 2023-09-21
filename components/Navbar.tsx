9ea9bf7892125fee3c84e77dc9acda52c725b8f6
NoShare16 <alex.wirschke14@gmail.com>
2023-09-21 14:42:21 +0200

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black'>
+      <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16'>
+        <Link href="/">
+          <Image
+            src={"/neuralNetwork.jpeg"}
+            alt='logo'
+            width={55}
+            height={40}
+          />
+        </Link>
+
+        <Image
+          src={"/hamburger-menu.svg"}
+          width={30}
+          height={30}
+          alt='Menu'
+          className='block md:hidden'
+        />
+
+        <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
+          <li className='body-text text-gradient_blue-purple !font-bold'>
+            <a href="https://www.alex-wirschke.com/" target='_blank'>
+              My Portfolio
+            </a>
+          </li>
+          <li className='body-text !font-normal'>
+            <a href="https://github.com/NoShare16" target='_blank'>
+              GitHub Profile
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
