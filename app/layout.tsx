85e47d3120026fdc1754a0a04407a27242eab271
NoShare16 <alex.wirschke14@gmail.com>
2023-09-25 21:47:54 +0200

    ceo improvements metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'AI Prompt Collection',
   description: 'Collection of hundreds of prompts for Ai',
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark only',
+    'twitter:image': 'https://ibb.co/Pjh9G1S',
+    'twitter:card': 'summary_large_image',
+    'og:url': 'https://ai-prompt-collection.vercel.app/',
+    'og:image': 'https://ibb.co/Pjh9G1S',
+    'og:type': 'website',
+  }
 }
 
 export default function RootLayout({
