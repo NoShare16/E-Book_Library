d333c1ef62de0b666d1c3aebf1b5084f3a5b5a16
NoShare16 <alex.wirschke14@gmail.com>
2023-09-25 21:13:50 +0200

    loading skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -3,9 +3,18 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const loading = () => {
   return (
-    <main>
-      <section>
-      <Skeleton className="w-[100px] h-[20px] rounded-full" />
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+       <Skeleton className="w-full h-[274px] rounded-lg" />
+      </section>
+
+      <section className="mt-6 flex w-full flex-col sm:mt-20">
+        <Skeleton className="h-10 w-56" />
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+        </div>
       </section>
     </main>
   )
