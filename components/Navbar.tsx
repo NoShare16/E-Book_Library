6768b603e21388ab8abb82881e74554288c488d7
NoShare16 <alex.wirschke14@gmail.com>
2023-09-27 13:02:08 +0200

    teyt gradient style changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className='flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black'>
-      <div className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16'>
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
         <Link href="/">
           <Image
             src={"/neuralNetwork.jpeg"}
-            alt='logo'
+            alt="logo"
             width={55}
             height={40}
           />
@@ -18,25 +18,25 @@ const Navbar = () => {
           src={"/hamburger-menu.svg"}
           width={30}
           height={30}
-          alt='Menu'
-          className='block md:hidden'
+          alt="Menu"
+          className="block md:hidden"
         />
 
-        <ul className='flex-center gap-x-3 max-md:hidden md:gap-x-10'>
-          <li className='body-text text-gradient_blue-purple !font-bold'>
-            <a href="https://www.alex-wirschke.com/" target='_blank'>
+        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+          <li className="body-text text-gradient_blue !font-bold">
+            <a href="https://www.alex-wirschke.com/" target="_blank">
               My Portfolio
             </a>
           </li>
-          <li className='body-text !font-normal'>
-            <a href="https://github.com/NoShare16" target='_blank'>
+          <li className="body-text !font-normal">
+            <a href="https://github.com/NoShare16" target="_blank">
               GitHub Profile
             </a>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
