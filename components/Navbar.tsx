23a63eb5fb5ae0b5f7996d808e42d83c6abb2a92
NoShare16 <alex.wirschke14@gmail.com>
2023-09-27 13:58:53 +0200

    added dropdown to navbar in mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
   return (
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 py-7 text-white bg-black">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
@@ -13,14 +17,6 @@ const Navbar = () => {
             height={40}
           />
         </Link>
-
-        <Image
-          src={"/hamburger-menu.svg"}
-          width={30}
-          height={30}
-          alt="Menu"
-          className="block md:hidden"
-        />
 
         <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
           <li className="body-text text-gradient_blue !font-bold">
@@ -34,7 +30,41 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
+
+        <Image
+          src={"/hamburger-menu.svg"}
+          width={30}
+          height={30}
+          alt="Menu"
+          className="block md:hidden"
+          onClick={() => setToggleDropdown((prev) => !prev)}
+        />
       </div>
+
+      {toggleDropdown && (
+        <div className="absolute right-0 top-full mt-3 p-5 rounded-lg bg-gray-300 min-w-[210px] flex flex-col gap-2 justify-center items-center">
+          <ul className="py-2 text-sm text-gray-700 ">
+            <li>
+              <a
+                href="https://www.alex-wirschke.com/"
+                target="_blank"
+                className="block px-4 py-2 hover:bg-gray-100 body-text !font-bold hover:rounded"
+              >
+                My Portfolio
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/NoShare16"
+                target="_blank"
+                className="block px-4 py-2 hover:bg-gray-100 body-text !font-normal hover:rounded"
+              >
+                GitHub Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
