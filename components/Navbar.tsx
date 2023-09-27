a01f9ac20e666b06906b0680b78ec670bd2e8850
NoShare16 <alex.wirschke14@gmail.com>
2023-09-27 16:24:09 +0200

    nav fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,32 +39,32 @@ const Navbar = () => {
           className="block md:hidden"
           onClick={() => setToggleDropdown((prev) => !prev)}
         />
-      </div>
 
-      {toggleDropdown && (
-        <div className="absolute right-0 top-full mt-3 p-5 rounded-lg bg-gray-300 min-w-[210px] flex flex-col gap-2 justify-center items-center">
-          <ul className="py-2 text-sm text-gray-700 ">
-            <li>
-              <a
-                href="https://www.alex-wirschke.com/"
-                target="_blank"
-                className="block px-4 py-2 hover:bg-gray-100 body-text !font-bold hover:rounded"
-              >
-                My Portfolio
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/NoShare16"
-                target="_blank"
-                className="block px-4 py-2 hover:bg-gray-100 body-text !font-normal hover:rounded"
-              >
-                GitHub Profile
-              </a>
-            </li>
-          </ul>
-        </div>
-      )}
+        {toggleDropdown && (
+          <div className="absolute right-0 top-full mt-3 p-5 rounded-lg bg-gray-300 min-w-[210px] flex flex-col gap-2 justify-center items-center md:hidden">
+            <ul className="py-2 text-sm text-gray-700 ">
+              <li>
+                <a
+                  href="https://www.alex-wirschke.com/"
+                  target="_blank"
+                  className="block px-4 py-2 hover:bg-gray-100 body-text !font-bold hover:rounded"
+                >
+                  My Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/NoShare16"
+                  target="_blank"
+                  className="block px-4 py-2 hover:bg-gray-100 body-text !font-normal hover:rounded"
+                >
+                  GitHub Profile
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
